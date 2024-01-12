27b3a4ab9ee72c52d571e76f3a432c5f901647ac
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2024-01-12 19:43:55 +0100

    correccion pais

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -41,7 +41,7 @@ const Formulario = () => {
             <option value="CR">Costa Rica</option>
             <option value="ES">España</option>
             <option value="PE">Peru</option>
-            <option value="VE">Italia</option>
+            <option value="IT">Italia</option>
           </select>
         </div>
         <input type="submit" value="Consultar Clima" />
